389583ff8a34a1577d8ff7fdc210ace67ec354be
gokhanus <gokhan.demir@pia-team.com>
2025-03-28 13:23:30 +0300

    - appended release notes - beautified README.md - updated block javadocs in the `TmfClient` interface

--- src/main/java/com/pia/tmf/common/client/api/TmfClient.java
+++ src/main/java/com/pia/tmf/common/client/api/TmfClient.java
@@ -12,97 +12,97 @@ import reactor.core.publisher.Mono;
  */
 public interface TmfClient<C, U, R> {
 
-  /** these get methods will first retrieve an accessToken */
+  /** These get methods first retrieve an access token */
   Mono<R> get(String id);
   Mono<R> get(String id, TmfRequestContext requestContext);
   <T> Mono<T> get(String id, Class<T> type);
   <T> Mono<T> get(String id, TmfRequestContext requestContext, Class<T> type);
 
-  /** these get methods uses the client provided accessToken */
+  /** These get methods use the client-provided access token */
   Mono<R> getWithToken(String token, String id);
   Mono<R> getWithToken(String token, String id, TmfRequestContext requestContext);
   <T> Mono<T> getWithToken(String token, String id, Class<T> type);
   <T> Mono<T> getWithToken(String token, String id, TmfRequestContext requestContext, Class<T> type);
 
-  /** the list methods serves one page at a time. This group will first retrieve an accessToken */
+  /** List methods that serve one page at a time (access token retrieved automatically) */
   Flux<R> list();
-  <T>Flux<T> list(Class<T> type);
+  <T> Flux<T> list(Class<T> type);
   Flux<R> list(Pageable request);
-  <T>Flux<T> list(Pageable request, Class<T> type);
+  <T> Flux<T> list(Pageable request, Class<T> type);
 
-  /** the list methods serves one page at a time. This group will use the provided accessToken */
+  /** List methods that serve one page at a time (using the provided access token) */
   Flux<R> listWithToken(String token);
-  <T>Flux<T> listWithToken(String token, Class<T> type);
+  <T> Flux<T> listWithToken(String token, Class<T> type);
   Flux<R> listWithToken(String token, Pageable request);
-  <T>Flux<T> listWithToken(String token, Pageable request, Class<T> type);
+  <T> Flux<T> listWithToken(String token, Pageable request, Class<T> type);
 
-  /** listAll methods retrieves all pages. This group will first retrieve an accessToken */
+  /** listAll methods retrieve all pages (access token retrieved automatically) */
   Flux<R> listAll();
-  <T>Flux<T> listAll(Class<T> type);
+  <T> Flux<T> listAll(Class<T> type);
   Flux<R> listAll(Pageable request);
-  <T>Flux<T> listAll(Pageable request, Class<T> type);
+  <T> Flux<T> listAll(Pageable request, Class<T> type);
 
-  /** listAll methods retrieves all pages. This group will use the provided accessToken */
+  /** listAll methods retrieve all pages (using the provided access token) */
   Flux<R> listAllWithToken(String token);
-  <T>Flux<T> listAllWithToken(String token, Class<T> type);
+  <T> Flux<T> listAllWithToken(String token, Class<T> type);
   Flux<R> listAllWithToken(String token, Pageable request);
-  <T>Flux<T> listAllWithToken(String token, Pageable request, Class<T> type);
+  <T> Flux<T> listAllWithToken(String token, Pageable request, Class<T> type);
 
-  /** these methods retrieve a page. This group will first retrieve an accessToken */
+  /** These methods retrieve a page (access token retrieved automatically) */
   Mono<TmfPage<Flux<R>>> listPaged();
-  <T>Mono<TmfPage<Flux<T>>> listPaged(Class<T> type);
+  <T> Mono<TmfPage<Flux<T>>> listPaged(Class<T> type);
   Mono<TmfPage<Flux<R>>> listPaged(Pageable request);
-  <T>Mono<TmfPage<Flux<T>>> listPaged(Pageable request, Class<T> type);
+  <T> Mono<TmfPage<Flux<T>>> listPaged(Pageable request, Class<T> type);
 
-  /** these methods retrieve a page. This group will use the provided accessToken */
+  /** These methods retrieve a page (using the provided access token) */
   Mono<TmfPage<Flux<R>>> listPagedWithToken(String token);
-  <T>Mono<TmfPage<Flux<T>>> listPagedWithToken(String token, Class<T> type);
+  <T> Mono<TmfPage<Flux<T>>> listPagedWithToken(String token, Class<T> type);
   Mono<TmfPage<Flux<R>>> listPagedWithToken(String token, Pageable request);
-  <T>Mono<TmfPage<Flux<T>>> listPagedWithToken(String token, Pageable request, Class<T> type);
+  <T> Mono<TmfPage<Flux<T>>> listPagedWithToken(String token, Pageable request, Class<T> type);
 
-  /** post a single object to create. This group will first retrieve an accessToken */
+  /** Post a single object to create (access token retrieved automatically) */
   Mono<R> post(C obj);
   Mono<R> post(C obj, TmfRequestContext requestContext);
   <T> Mono<T> post(C obj, Class<T> type);
   <T> Mono<T> post(C obj, TmfRequestContext requestContext, Class<T> type);
 
-  /** post a single object to create. This group will use the provided accessToken */
+  /** Post a single object to create (using the provided access token) */
   Mono<R> postWithToken(String token, C obj);
   Mono<R> postWithToken(String token, C obj, TmfRequestContext requestContext);
   <T> Mono<T> postWithToken(String token, C obj, Class<T> type);
   <T> Mono<T> postWithToken(String token, C obj, TmfRequestContext requestContext, Class<T> type);
 
-  /** merge patch a single object by its id. This group will first retrieve an accessToken */
+  /** Merge patch a single object by its id (access token retrieved automatically) */
   Mono<R> patch(String id, U obj);
   Mono<R> patch(String id, U obj, TmfRequestContext requestContext);
   <T> Mono<T> patch(String id, U obj, Class<T> type);
   <T> Mono<T> patch(String id, U obj, TmfRequestContext requestContext, Class<T> type);
 
-  /** merge patch a single object by its id. This group will use the provided accessToken */
+  /** Merge patch a single object by its id (using the provided access token) */
   Mono<R> patchWithToken(String token, String id, U obj);
   Mono<R> patchWithToken(String token, String id, U obj, TmfRequestContext requestContext);
   <T> Mono<T> patchWithToken(String token, String id, U obj, Class<T> type);
   <T> Mono<T> patchWithToken(String token, String id, U obj, TmfRequestContext requestContext, Class<T> type);
-  
-  /** json patch a single object by its id. This group will first retrieve an accessToken */
+
+  /** JSON patch a single object by its id (access token retrieved automatically) */
   Mono<R> patch(String id, JsonPatch jsonPatch);
   Mono<R> patch(String id, JsonPatch jsonPatch, TmfRequestContext requestContext);
   <T> Mono<T> patch(String id, JsonPatch jsonPatch, Class<T> type);
   <T> Mono<T> patch(String id, JsonPatch jsonPatch, TmfRequestContext requestContext, Class<T> type);
 
-  /** json patch a single object by its id. This group will use the provided accessToken */
+  /** JSON patch a single object by its id (using the provided access token) */
   Mono<R> patchWithToken(String token, String id, JsonPatch jsonPatch);
   Mono<R> patchWithToken(String token, String id, JsonPatch jsonPatch, TmfRequestContext requestContext);
   <T> Mono<T> patchWithToken(String token, String id, JsonPatch jsonPatch, Class<T> type);
   <T> Mono<T> patchWithToken(String token, String id, JsonPatch jsonPatch, TmfRequestContext requestContext, Class<T> type);
 
-  /** delete a single object by its id. This group will first retrieve an accessToken */
+  /** Delete a single object by its id (access token retrieved automatically) */
   Mono<Void> delete(String id);
   Mono<Void> delete(String id, TmfRequestContext requestContext);
   <T> Mono<T> delete(String id, Class<T> type);
   <T> Mono<T> delete(String id, Class<T> type, TmfRequestContext requestContext);
 
-  /** delete a single object by its id. This group will use the provided accessToken */
+  /** Delete a single object by its id (using the provided access token) */
   Mono<Void> deleteWithToken(String token, String id);
   Mono<Void> deleteWithToken(String token, String id, TmfRequestContext requestContext);
   <T> Mono<T> deleteWithToken(String token, String id, Class<T> type);
